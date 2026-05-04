2f3f540501b0bdbdeae0661f888adfc9b22a7271
blackadi <odai.shalabi@gmail.cim>
2026-05-04 14:07:50 +0300

    migrate from class compoment to functional component

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,54 +1,47 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import CardList from "../components/CardList.jsx";
 import SearchBox from "../components/SearchBox.jsx";
 import Scroll from "../components/Scroll.jsx";
 import { kittens } from "../kittens.js";
+const App = () => {
+  const [kittens, setKittens] = useState([]);
+  const [searchfield, setSearchField] = useState("");
 
-class App extends Component {
-  constructor() {
-    super();
+  useEffect(() => {
+    async function fetchData() {
+      const respose = await fetch("https://jsonplaceholder.typicode.com/users");
+      const kittens = await respose.json();
+      setKittens(kittens);
+    }
+    fetchData();
+  }, []);
 
-    this.state = {
-      kittens: [],
-      searchfield: "",
-    };
+  function onSearchChange(event) {
+    setSearchField(event.target.value);
   }
 
-  async componentDidMount() {
-    const respose = await fetch("https://jsonplaceholder.typicode.com/users");
-    const kittens = await respose.json();
-    this.setState({ kittens });
-  }
+  const filteredKittens = kittens.filter((kitten) => {
+    return kitten.name.toLowerCase().includes(searchfield.toLowerCase());
+  });
 
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value });
-  };
-
-  render() {
-    const { kittens, searchfield } = this.state;
-    const filteredKittens = kittens.filter((kitten) => {
-      return kitten.name.toLowerCase().includes(searchfield.toLowerCase());
-    });
-
-    return !kittens.length ? (
-      <div className="flex items-center justify-center pt5">
-        <h1>Loading</h1>
-        <div className="waving-loader flex items-center">
-          <div className="dot bg-dark-gray br-100 h1 w1 mh1"></div>
-          <div className="dot bg-dark-gray br-100 h1 w1 mh1"></div>
-          <div className="dot bg-dark-gray br-100 h1 w1 mh1"></div>
-        </div>
+  return !kittens.length ? (
+    <div className="flex items-center justify-center pt5">
+      <h1>Loading</h1>
+      <div className="waving-loader flex items-center">
+        <div className="dot bg-dark-gray br-100 h1 w1 mh1"></div>
+        <div className="dot bg-dark-gray br-100 h1 w1 mh1"></div>
+        <div className="dot bg-dark-gray br-100 h1 w1 mh1"></div>
       </div>
-    ) : (
-      <div>
-        <h1 className="f2">KittenFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <Scroll>
-          <CardList kittens={filteredKittens} />
-        </Scroll>
-      </div>
-    );
-  }
-}
+    </div>
+  ) : (
+    <div>
+      <h1 className="f2">KittenFriends</h1>
+      <SearchBox searchChange={onSearchChange} />
+      <Scroll>
+        <CardList kittens={filteredKittens} />
+      </Scroll>
+    </div>
+  );
+};
 
 export default App;
